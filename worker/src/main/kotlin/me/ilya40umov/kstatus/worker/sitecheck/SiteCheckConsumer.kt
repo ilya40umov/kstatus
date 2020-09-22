49fb9bedbad9316122492df5b8d788f4af6747d5
Illia Sorokoumov <illia.sorokoumov@gmail.com>
2020-09-22 22:26:08 +0200

    Don't extend CoroutineScope in consumer

--- worker/src/main/kotlin/me/ilya40umov/kstatus/worker/sitecheck/SiteCheckConsumer.kt
+++ worker/src/main/kotlin/me/ilya40umov/kstatus/worker/sitecheck/SiteCheckConsumer.kt
@@ -1,17 +1,10 @@
 package me.ilya40umov.kstatus.worker.sitecheck
 
-import kotlinx.coroutines.CancellationException
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.*
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.channels.ReceiveChannel
 import kotlinx.coroutines.channels.SendChannel
 import kotlinx.coroutines.future.await
-import kotlinx.coroutines.isActive
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.yield
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
 import me.ilya40umov.kstatus.site.events.SiteCheckRequested
@@ -19,21 +12,18 @@ import me.ilya40umov.kstatus.worker.SqsMessage
 import mu.KotlinLogging
 import software.amazon.awssdk.services.sqs.SqsAsyncClient
 import software.amazon.awssdk.services.sqs.model.ReceiveMessageRequest
-import kotlin.coroutines.CoroutineContext
 
 private val logger = KotlinLogging.logger {}
 
 class SiteCheckConsumer(
-    private val config: SiteCheckConfig,
-    private val sqsClient: SqsAsyncClient,
-    private val handler: SiteCheckHandler
-) : CoroutineScope {
-
+        private val config: SiteCheckConfig,
+        private val sqsClient: SqsAsyncClient,
+        private val handler: SiteCheckHandler
+) {
     private val supervisorJob = SupervisorJob()
-    override val coroutineContext: CoroutineContext
-        get() = Dispatchers.IO + supervisorJob
+    private val scope = CoroutineScope(Dispatchers.IO + supervisorJob)
 
-    fun start(): Job = launch {
+    fun start(): Job = scope.launch {
         val messageChannel = Channel<SqsMessage<SiteCheckRequested>>()
         repeat(config.processors) { launchProcessor(messageChannel) }
         launchSqsPoller(messageChannel)
@@ -46,10 +36,10 @@ class SiteCheckConsumer(
     private fun CoroutineScope.launchSqsPoller(channel: SendChannel<SqsMessage<SiteCheckRequested>>) = launch {
         repeatUntilCancelled {
             val receiveRequest = ReceiveMessageRequest.builder()
-                .queueUrl(config.sqsQueueUrl)
-                .waitTimeSeconds(SQS_WAIT_TIME_SECONDS)
-                .maxNumberOfMessages(SQS_MAX_MESSAGES)
-                .build()
+                    .queueUrl(config.sqsQueueUrl)
+                    .waitTimeSeconds(SQS_WAIT_TIME_SECONDS)
+                    .maxNumberOfMessages(SQS_MAX_MESSAGES)
+                    .build()
 
             val response = sqsClient.receiveMessage(receiveRequest).await()
             if (response.hasMessages()) {
@@ -73,10 +63,12 @@ class SiteCheckConsumer(
                 } catch (e: Exception) {
                     logger.error { "Failed to process: $message" }
                 } finally {
-                    sqsClient.deleteMessage { req ->
-                        req.queueUrl(config.sqsQueueUrl)
-                        req.receiptHandle(message.receiptHandle)
-                    }.await()
+                    withContext(NonCancellable) {
+                        sqsClient.deleteMessage { req ->
+                            req.queueUrl(config.sqsQueueUrl)
+                            req.receiptHandle(message.receiptHandle)
+                        }.await()
+                    }
                 }
             }
         }
